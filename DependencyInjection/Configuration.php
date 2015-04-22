90f2576bb2a731c71d0b1c7f2e4d547eb2988be4
srijana <srijana@crossovernepal.com>
2015-04-22 12:34:44 +0545

    modified namespace

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace UAM\Bundle\UAMMaintenanceBundle\DependencyInjection;
+namespace UAM\Bundle\MaintenanceBundle\DependencyInjection;
 
 use Symfony\Component\Config\Definition\Builder\TreeBuilder;
 use Symfony\Component\Config\Definition\ConfigurationInterface;
--- UAMMaintenanceBundle.php
+++ UAMMaintenanceBundle.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace UAM\Bundle\UAMMaintenanceBundle;
+namespace UAM\Bundle\MaintenanceBundle;
 
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
