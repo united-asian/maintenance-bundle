b3627981961df5f628c74c4c8ef6ec35feca970f
srijana <srijana@crossovernepal.com>
2015-04-24 17:05:29 +0545

    modified contents

--- Controller/MaintenanceController.php
+++ Controller/MaintenanceController.php
@@ -1,24 +1,105 @@
 <?php
-namespace MaintenanceBundle\Controller;
+namespace UAM\Bundle\MaintenanceBundle\Controller;
 
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
+use Model\UAMMaintenanceQuery;
 
 /**
  * @Route("/maintenance")
 */
 class MaintenanceController extends Controller
 {
-/**
- * @Route("/")
-*/
+    /**
+     * @Route("/")
+    */
     public function indexAction(Request $request)
     {
         return array();
     }
+    /**
+     * @Route("/list", name="maintenance_record_list", requirements={"_format": "json"}, defaults={"_format": "json"})
+     * @Template()
+    */
+    public function listAction(Request $request)
+    {
+        $query = $this->getQuery();
+
+        $total_count = $query
+            ->count();
+
+        $query->filter(
+            $this->getFilters($request),
+            $this->getSearchColumns()
+        );
+
+        $filtered_count = $query
+            ->count();
+
+        $limit = $this->getLimit($request);
+        $offset = $this->getOffset($request);
+
+        $persons = $query
+            ->sort($this->getSortOrder($request))
+            ->setLimit($limit)
+            ->setOffset($offset)
+            ->find();
+
+        return array(
+            'total_count' => $total_count,
+            'filtered_count' => $filtered_count,
+            'maintenance_records' => $maintenance_records
+        );
+    }
+
+    protected function getQuery()
+    {
+        return UAMManitenanceQuery::create();
+    }
+
+    protected function getLimit(Request $request)
+    {
+        return min(100, $request->query->get('length', 10));
+    }
+
+    protected function getLimit(Request $request)
+    {
+        return min(100, $request->query->get('length', 10));
+    }
+
+    protected function getOffset(Request $request)
+    {
+        return max($request->query->get('start', 0), 0);
+    }
+
+    protected function getFilters(Request $request)
+    {
+        return array();
+    }
+
+    protected function getSearchColumns()
+    {
+        return array();
+    }
+
+    protected function getSortOrder(Request $request)
+    {
+        return array();
+    }
+
+    protected function getSortColumns()
+    {
+        return array();
+    }
+    /**
+     * @Route("/{id}", name="maintenance_record_edit")
+     * @Template()
+    */
+    public function editAction(Request $request, $id)
+    {
+        return array();
+    }
 }
-
-
--- Model/UAMMaintenanceQuery.php
+++ Model/UAMMaintenanceQuery.php
@@ -6,4 +6,14 @@ use Model\om\BaseUAMMaintenanceQuery;
 
 class UAMMaintenanceQuery extends BaseUAMMaintenanceQuery
 {
+
+    public function filter(array $filters = null, array $columns = array())
+    {
+        return $this;
+    }
+
+    public function sort(array $order = array())
+    {
+        return $this;
+    }
 }
