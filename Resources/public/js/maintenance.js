931be944a933daaf9010b348bc2e99708c2487bc
srijana <srijana@crossovernepal.com>
2015-05-12 11:19:37 +0545

    added js file

---
+++ Resources/public/js/maintenance.js
@@ -0,0 +1,85 @@
+
+!function( $ ) {
+	$.fn.maintenance = function( method ) {
+
+		var settings,
+			table;
+
+		// Public methods
+		var methods = {
+			init: function( options ) {
+				settings = $.extend( true, {}, $.fn.maintenance.defaults, options );
+
+				return this.each(function() {
+					var $this = $( this );
+
+					table = $( "table.table", this ).dataTable( $.extend( true, {}, settings.datatables, {
+			initComplete: function( settings, json ) {
+   				$( settings.nTable ).css( "display", "table" );
+			}
+					} ) );
+				});
+			}
+		};
+
+		if ( methods[ method ] ) {
+			return methods[ method ].apply( this, Array.prototype.slice.call( arguments, 1 ) );
+		}
+		else if ( typeof method === "object" || !method ) {
+			return methods.init.apply( this, arguments );
+		}
+		else {
+			$.error( "Method " +  method + " does not exist in $.maintenance." );
+		}
+	};
+
+	$.fn.maintenance.defaults = {
+		datatables: {
+			autoWidth: false,
+			columns: [
+				{ data: null },
+				{ data: "date_start" },
+				{ data: "date_end" },
+				{ data: "description" },
+				{ data: "confirmed" }
+				{ data: null }
+			],
+			columnDefs: [
+				{ orderable: false, targets: [ 0, 5 ] }, // First column and last column ("actions") are not sortable
+				{ visible: false, targets: [ 0 ] }, // Hide the first column
+				{ className: "actions", targets: [ 5 ] } // Set special "actions" class on cells in the last column
+			],
+			language: {
+				url: "/bundles/uamdatatables/lang" + persons.locale + ".json"
+			},
+			orderable: true,
+			paging: true,
+			processing: true,
+			serverSide: true, // set datatables to use ajax to display content
+			stripeClasses: []
+		}
+	};
+} ( window.jQuery );
+
+// Make sure that a parent element of the table has the class ''.persons'' set so that this plugin can be triggered properly when the page is loaded.
+$( document ).ready(function() {
+	$( ".maintenance" ).maintenance( maintenance );
+});
+
+Because datatables uses ajax, it needs to be told which url to call. For this, datatables expects a ajax.url option. This url typically depends on the environment, which means it is computed by symfony in PHP. To pass PHP values to javascript, we use the following pattern: In each page template a javascript variable is defined (typically named after the page's contents) ; this variable holds values computed via PHP. The variable is then passed to the page's plugin when it is invoked.
+
+{% block foot_script %}
+	{{ parent() }}
+	<javascripts>
+	...
+	<endjavascripts>
+	<script>
+	maintenance = {
+		datatables: {
+			ajax: {
+				url: "{{ path('maintenance_record') }}"
+			}
+		}
+	};
+	</script>
+{% endblock head_script %}
