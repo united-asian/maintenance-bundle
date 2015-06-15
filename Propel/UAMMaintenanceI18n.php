63c8334a3c26ba31c6c691a95200517dd9b394e7
srijana <srijana@crossovernepal.com>
2015-06-15 15:56:53 +0545

    generated model

---
+++ Propel/UAMMaintenanceI18n.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace UAM\Bundle\MaintenanceBundle\Propel;
+
+use UAM\Bundle\MaintenanceBundle\Propel\om\BaseUAMMaintenanceI18n;
+
+class UAMMaintenanceI18n extends BaseUAMMaintenanceI18n
+{
+}
