2268473918572f9a83a9ad3589b0eb12370545ba
srijana <srijana@crossovernepal.com>
2015-06-16 14:07:24 +0545

    modified class name

--- Propel/UAMMaintenanceI18nQuery.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace UAM\Bundle\MaintenanceBundle\Propel;
-
-use UAM\Bundle\MaintenanceBundle\Propel\om\BaseUAMMaintenanceI18nQuery;
-
-class UAMMaintenanceI18nQuery extends BaseUAMMaintenanceI18nQuery
-{
-}
