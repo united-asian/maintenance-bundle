fb343759637e066c90bde56058ea5e614743e8f3
srijana <srijana@crossovernepal.com>
2015-06-16 17:50:52 +0545

    added logic

--- EventListener/WarningRequestListener.php
+++ EventListener/WarningRequestListener.php
@@ -2,12 +2,29 @@
 namespace UAM\Bundle\MaintenanceBundle\EventListener;
 
 use Symfony\Component\HttpKernel\Event\GetResponseEvent;
-use Symfony\Component\HttpKernel\HttpKernel;
+use UAM\Bundle\MaintenanceBundle\Propel\MaintenanceQuery;
 
 class WarningRequestListener
 {
     public function onKernelRequest(GetResponseEvent $event)
     {
+        $current_date = date("Y/m/d");
 
+        $maintenance = MaintenanceQuery::create()
+            ->filterByDateStart(array('max' => $current_date ))
+            ->orderByDateStart('desc')
+            ->filterByConfirmed($confirmed = true)
+            ->findOne();
+
+        $date_start = $maintenance->getDateStart();
+        $date_end = $maintenance->getDateEnd();
+
+        if (($current_date >= $date_start ) && ($current_date <= $date_end))
+        {
+            $adminController = $this->get('admin.controller')->progress($array);
+        }
+        else{
+        return;
+        }
     }
 }
