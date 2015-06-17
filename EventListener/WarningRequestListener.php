19bbb5e3f55a4a4a98937086d8268c483dfb4d0d
srijana <srijana@crossovernepal.com>
2015-06-17 12:12:21 +0545

    set response

--- EventListener/WarningRequestListener.php
+++ EventListener/WarningRequestListener.php
@@ -1,14 +1,24 @@
 <?php
 namespace UAM\Bundle\MaintenanceBundle\EventListener;
 
+use DateTime;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpKernel\Event\GetResponseEvent;
+use Symfony\Component\Routing\RouterInterface;
 use UAM\Bundle\MaintenanceBundle\Propel\MaintenanceQuery;
 
 class WarningRequestListener
 {
+    protected $router;
+
+    public function __construct(RouterInterface $router)
+    {
+        $this->router = $router;
+    }
+
     public function onKernelRequest(GetResponseEvent $event)
     {
-        $current_date = date("Y/m/d");
+        $current_date = new DateTime();
 
         $maintenance = MaintenanceQuery::create()
             ->filterByDateStart(array('max' => $current_date ))
@@ -19,12 +29,17 @@ class WarningRequestListener
         $date_start = $maintenance->getDateStart();
         $date_end = $maintenance->getDateEnd();
 
-        if (($current_date >= $date_start ) && ($current_date <= $date_end))
-        {
-            $adminController = $this->get('admin.controller')->progress($array);
-        }
-        else{
-        return;
+
+        if (($current_date >= $date_start ) && ($current_date <= $date_end)) {
+            $route = 'uam_maintenance_admin_progress';
+
+            $request_url = $event->getRequest()->getUri();
+
+            $url = $this->router->generate($route, array(), true);
+
+            if( $request_url != $url) {
+                $event->setResponse(new RedirectResponse($url));
+            }
         }
     }
 }
