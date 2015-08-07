76d7023e1ab47d25ac9e4885907a2a8812b9eb91
uid=narayan <narayan@crossovernepal.com>
2015-08-07 14:40:30 +0545

    refactored listener

--- EventListener/WarningRequestListener.php
+++ EventListener/WarningRequestListener.php
@@ -3,9 +3,14 @@
 namespace UAM\Bundle\MaintenanceBundle\EventListener;
 
 use DateTime;
+use Symfony\Bundle\AsseticBundle\Controller\AsseticController;
+use Symfony\Bundle\WebProfilerBundle\Controller\ProfilerController;
 use Symfony\Component\HttpFoundation\RedirectResponse;
-use Symfony\Component\HttpKernel\Event\GetResponseEvent;
+use Symfony\Component\HttpKernel\Event\FilterControllerEvent;
+use Symfony\Component\HttpKernel\Event\GetResponseForExceptionEvent;
 use Symfony\Component\Routing\RouterInterface;
+use UAM\Bundle\MaintenanceBundle\Controller\MaintenanceController;
+use UAM\Bundle\MaintenanceBundle\Exceptions\AppUnderMaintenanceException;
 use UAM\Bundle\MaintenanceBundle\Propel\MaintenanceQuery;
 
 class WarningRequestListener
@@ -17,8 +22,20 @@ class WarningRequestListener
         $this->router = $router;
     }
 
-    public function onKernelRequest(GetResponseEvent $event)
+    public function onKernelController(FilterControllerEvent $event)
     {
+        $controller = $event->getController();
+
+        if (!is_array($controller)) {
+            return;
+        }
+
+        if ($controller[0] instanceof AsseticController ||
+            $controller[0] instanceof ProfilerController ||
+            $controller[0] instanceof MaintenanceController) {
+            return;
+        }
+
         $current_date = new DateTime();
 
         $maintenance = MaintenanceQuery::create()
@@ -28,14 +45,20 @@ class WarningRequestListener
             ->findOne();
 
         if ($maintenance) {
-            $route = 'uam_maintenance_progress';
-
-            $request_url = $event->getRequest()->getUri();
-            $url = $this->router->generate($route, array(), true);
-
-            if ($request_url != $url) {
-                $event->setResponse(new RedirectResponse($url));
-            }
+            throw new AppUnderMaintenanceException('App under maintenance');
         }
+    }
+
+    public function onKernelException(GetResponseForExceptionEvent $event)
+    {
+        $exception = $event->getException();
+
+        if (!($exception instanceof AppUnderMaintenanceException)) {
+            return;
+        }
+
+        $response = new RedirectResponse($this->router->generate('uam_maintenance_progress'));
+
+        $event->setResponse($response);
     }
 }
---
+++ Exceptions/AppUnderMaintenanceException.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace UAM\Bundle\MaintenanceBundle\Exceptions;
+
+use RuntimeException;
+
+class AppUnderMaintenanceException extends RuntimeException
+{
+}
