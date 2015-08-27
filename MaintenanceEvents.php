76a5751d54e720d0498e24560382e14abd019fa0
uid=narayan <narayan@crossovernepal.com>
2015-08-27 13:24:09 +0545

    added MaintenanceEvents class

---
+++ MaintenanceEvents.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace UAM\Bundle\MaintenanceBundle;
+
+final class MaintenanceEvents
+{
+    const RECORD_CREATED = 'maintenance.record.created';
+    const RECORD_CONFIRMED = 'maintenance.record.confirmed';
+    const RECORD_DELETED = 'maintenance.record.deleted';
+}
