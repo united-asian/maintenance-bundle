e9183fde26b94823814c48b15987a629aa0976e8
uid=narayan <narayan@crossovernepal.com>
2015-08-27 14:02:17 +0545

    updated maintenence event class

--- Event/MaintenanceEvent.php
+++ Event/MaintenanceEvent.php
@@ -3,7 +3,19 @@
 namespace UAM\Bundle\MaintenanceBundle\Event;
 
 use Symfony\Component\EventDispatcher\Event;
+use UAM\Bundle\MaintenanceBundle\Propel\Maintenance;
 
 class MaintenanceEvent extends Event
 {
+    protected $maintenance;
+
+    public function __construct(Maintenance $maintenance)
+    {
+        $this->task = $maintenance;
+    }
+
+    public function getMaintenance()
+    {
+        return $this->task;
+    }
 }
